7adf540f78641f3380c810035dfb1bf87f0cc717
Marcelo Jeronymo <jeronymos@hotmail.com>
2022-09-28 18:52:21 -0300

    Criando Class Livro

--- src/br/com/dio/ProgramaIntellij.java
+++ src/br/com/dio/ProgramaIntellij.java
@@ -8,12 +8,52 @@ public class ProgramaIntellij {
 
         System.out.println(Coelho);
 
+        Livro Livro = new Livro("O problema dos Corpos", 200);
+        System.out.println(Livro);
+
+
 
 
       /*  int a = 5;
         int b = 3;
         System.out.println("Hello Marcelo!" + (a+b));*/
 
-
     }
 }
+
+    class Livro {
+    private String nome;
+    private Integer nupaginas;
+
+        public Livro(String nome, Integer nupaginas) {
+            this.nome = nome;
+            this.nupaginas = nupaginas;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public Integer getNupaginas() {
+            return nupaginas;
+        }
+
+        public void setNupaginas(Integer nupaginas) {
+            this.nupaginas = nupaginas;
+        }
+
+        @Override
+        public String toString() {
+            return "Livro{" +
+                    "nome='" + nome + '\'' +
+                    ", nupaginas=" + nupaginas +
+                    '}';
+        }
+    }
+
