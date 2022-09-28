eb6d7d7064ea063411a9b2de7db9d5dfcec2487e
Marcelo Jeronymo <jeronymos@hotmail.com>
2022-09-28 18:35:18 -0300

    First Commit

---
+++ src/br/com/dio/ProgramaIntellij.java
@@ -0,0 +1,19 @@
+package br.com.dio;
+
+import br.com.dio.model.coelho;
+
+public class ProgramaIntellij {
+    public static void main(String[] args) {
+        coelho Coelho = new coelho();
+
+        System.out.println(Coelho);
+
+
+
+      /*  int a = 5;
+        int b = 3;
+        System.out.println("Hello Marcelo!" + (a+b));*/
+
+
+    }
+}
